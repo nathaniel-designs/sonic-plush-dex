af61cc166f8f0edc229b425025e5a6db267ac99c
Destin Augustin <122779357+nathaniel-designs@users.noreply.github.com>
2025-08-06 13:45:38 -0400

    Adding docker support for isolated environments to run any of the code.

--- app/database.py
+++ app/database.py
@@ -2,6 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .config import URL_DATABASE
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+URL_DATABASE = os.getenv("DATABASE_URL")
 
 engine = create_engine(URL_DATABASE)
 
